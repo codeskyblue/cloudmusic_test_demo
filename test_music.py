7dd887d5ca76f77b334b405a25d6c7a9f653a250
codeskyblue <codeskyblue@gmail.com>
2019-09-28 13:03:11 +0800

    init cases

--- test_music.py
+++ test_music.py
@@ -2,7 +2,54 @@
 #
 
 import uiautomator2 as u2
+import time
+import pytest
 
 
 def test_login():
     print("Everything is going")
+
+
+@pytest.fixture(scope="session")
+def device():
+    d = u2.connect()
+    d.xpath.when("服务条款和隐私政策提示").when("同意").click()
+    d.xpath.when("拒绝").when("允许").click()
+    # d.xpath.when("@com.netease.cloudmusic:id/aug").click()  # 同意
+
+    d.xpath.watch_background(interval=1)
+    d.xpath.global_set("timeout", 30)
+    return d
+
+
+@pytest.fixture
+def d(device):
+    _d = device
+    _d.app_clear("com.netease.cloudmusic")
+    _d.app_start("com.netease.cloudmusic")
+
+    _d(text="立即体验").wait(timeout=20)
+    time.sleep(1)
+    _d.click(0.261, 0.96)
+    _d(text="立即体验").click()
+
+    return _d
+
+
+def test_每日歌曲(d: u2.Session):
+    d.xpath('//*[@text="每日推荐"]').click()
+    d.xpath('播放全部').click()
+    assert d.xpath('//*[@content-desc="分享"]').wait()
+    assert d.xpath("播放暂停").exists
+    assert d.xpath("下一首").exists
+
+
+def test_搜索播放音乐(d: u2.Session):
+    d.xpath('//*[@content-desc="搜索"]').click()
+    d.xpath(
+        '//*[@resource-id="com.netease.cloudmusic:id/search_src_text"]').click()  # 搜索框
+    d.send_keys("水手", clear=True)
+    d.send_action("search")
+    d.xpath(
+        '//*[@resource-id="com.netease.cloudmusic:id/a0w" and @text="水手"]').click()
+    assert d.xpath("播放暂停").wait()
